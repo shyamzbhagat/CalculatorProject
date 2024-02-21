37faac6316c55104b8fa08e28821187653ea43e1
shyamzbhagat <shyamzbhagat@outlook.com>
2024-02-21 15:47:18 +0530

    Added division functionality

--- Program.cs
+++ Program.cs
@@ -13,6 +13,10 @@ static int MultiplyNumbers(int num1, int num2)
     return num1 * num2;
 }
 
+static double DivideNumbers(int num1, int num2)
+{
+    return (double)num1 / num2;
+}
 
 // Greet the user
 Console.Write("Please enter your name: ");
@@ -30,10 +34,10 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Choose an operation:");
 Console.WriteLine("1. Add");
 Console.WriteLine("2. Subtract");
-Console.WriteLine("3. Multiply"); // New option for multiplication
-Console.Write("Enter your choice (1, 2, or 3): "); // Updated prompt
+Console.WriteLine("3. Multiply");
+Console.WriteLine("4. Divide"); 
+Console.Write("Enter your choice (1, 2, 3, or 4): ");
 int operation = Convert.ToInt32(Console.ReadLine());
-
 
 switch (operation)
 {
@@ -45,9 +49,17 @@ switch (operation)
         int difference = SubtractNumbers(number1, number2);
         Console.WriteLine($"The difference of {number1} and {number2} is {difference}.");
         break;
-    case 3: // Case for multiplication
+    case 3:
         int product = MultiplyNumbers(number1, number2);
         Console.WriteLine($"The product of {number1} and {number2} is {product}.");
+        break;
+    case 4:
+        if (number2 != 0) {
+            double quotient = DivideNumbers(number1, number2);
+            Console.WriteLine($"The quotient of {number1} divided by {number2} is {quotient}.");
+        } else {
+            Console.WriteLine("Cannot divide by zero.");
+        }
         break;
     default:
         Console.WriteLine("Invalid choice.");
