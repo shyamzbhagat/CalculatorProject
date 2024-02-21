89dae42b6c20da98f31565794b274ff0c6f538b4
shyamzbhagat <shyamzbhagat@outlook.com>
2024-02-21 14:46:52 +0530

    Added basic arithmetic functionality

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Please enter your name: ");
+string userName = Console.ReadLine();
+Console.WriteLine($"Hello, {userName}!");
+
+// Perform addition operation
+Console.Write("Enter the first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int sum = AddNumbers(number1, number2);
+Console.WriteLine($"The sum of {number1} and {number2} is {sum}.");
