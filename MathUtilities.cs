da66cbaa415aeeb26ca2755d350554d726dcf6bf
shyamzbhagat <shyamzbhagat@outlook.com>
2024-02-22 10:15:11 +0530

    Final changes

---
+++ MathUtilities.cs
@@ -0,0 +1,25 @@
+namespace CalculatorApp
+{
+    public static class MathUtilities
+    {
+        public static int AddNumbers(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int SubtractNumbers(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int MultiplyNumbers(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double DivideNumbers(int num1, int num2)
+        {
+            return (double)num1 / num2;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,36 +1,15 @@
-﻿static int AddNumbers(int num1, int num2)
-{
-    return num1 + num2;
-}
+﻿using CalculatorApp;
 
-static int SubtractNumbers(int num1, int num2)
-{
-    return num1 - num2;
-}
-
-static int MultiplyNumbers(int num1, int num2)
-{
-    return num1 * num2;
-}
-
-static double DivideNumbers(int num1, int num2)
-{
-    return (double)num1 / num2;
-}
-
-// Greet the user
 Console.Write("Please enter your name: ");
 string userName = Console.ReadLine();
 Console.WriteLine($"Hello, {userName}!");
 
-// Ask for numbers
 Console.Write("Enter the first number: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Enter the second number: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-// Update the user prompt and switch statement to include multiplication
 Console.WriteLine("Choose an operation:");
 Console.WriteLine("1. Add");
 Console.WriteLine("2. Subtract");
@@ -42,20 +21,20 @@ int operation = Convert.ToInt32(Console.ReadLine());
 switch (operation)
 {
     case 1:
-        int sum = AddNumbers(number1, number2);
+        int sum = MathUtilities.AddNumbers(number1, number2);
         Console.WriteLine($"The sum of {number1} and {number2} is {sum}.");
         break;
     case 2:
-        int difference = SubtractNumbers(number1, number2);
+        int difference = MathUtilities.SubtractNumbers(number1, number2);
         Console.WriteLine($"The difference of {number1} and {number2} is {difference}.");
         break;
     case 3:
-        int product = MultiplyNumbers(number1, number2);
+        int product = MathUtilities.MultiplyNumbers(number1, number2);
         Console.WriteLine($"The product of {number1} and {number2} is {product}.");
         break;
     case 4:
         if (number2 != 0) {
-            double quotient = DivideNumbers(number1, number2);
+            double quotient = MathUtilities.DivideNumbers(number1, number2);
             Console.WriteLine($"The quotient of {number1} divided by {number2} is {quotient}.");
         } else {
             Console.WriteLine("Cannot divide by zero.");
